6578df86b97d034e3f77aaa1aba911de49da038d
Raghad Altheeb <raghaddolat2@gmail.com>
2026-08-22 16:45:33 +0300

    Update logic_gate.py

---
+++ 01_Foundations_and_Syntax/Tasks/task_1_3_logic_gate/logic_gate.py
@@ -0,0 +1,28 @@
+gates= input("chose AND, OR,NOT,XOR: ").strip().upper()
+a= bool(int(input("enter value for A (1 or 0)"))).strip()
+b= bool(int(input("enter value for B (1 or 0)"))).strip()
+if gates != "NOT":
+    
+    b = bool(int(input_b_str))
+else:
+    b = None
+
+if gates == "AND":
+    reslt= a and b
+    print(f"AND Gate Output: {result}")
+
+elif gates == "OR":
+    reslt= a or b
+    print(f"OR Gate Output: {result}")
+
+elif gates == "NOT":
+    result = not a
+    print(f"NOT Gate Output: {result}")
+
+elif gates == "XOR":
+    reslt= a != b
+    print(f"XOR Gate Output: {result}")
+    
+else:
+    print("Error: Unrecognized gate selected.")
+  
